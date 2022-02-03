56b902e93ecd0f85b4c048707ac13659fbfc1a09
smv <esemve@gmail.com>
2022-02-03 21:31:01 +0100

    Buefy installed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <page-header />
     <router-view/>
   </div>
 </template>
 
 <script>
+import PageHeader from './components/PageHeader'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: { PageHeader }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
